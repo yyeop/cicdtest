f02a7d36544dde7bff5a6620089796d4ac638d5c
yunyeop <yykim000922@gmail.com>
2022-02-27 23:08:11 +0900

    initial commit

---
+++ src/main/java/com/example/giactest/GiactestApplication.java
@@ -0,0 +1,13 @@
+package com.example.giactest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiactestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GiactestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/giactest/GiactestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.giactest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiactestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
